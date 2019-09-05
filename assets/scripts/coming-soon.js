6c958a1fc13c0d1a7732955d6d40803070f750aa
ranjith <ranjithpmankada@gmail.com>
2019-09-05 09:18:05 +0530

    Delete scripts

--- assets/scripts/coming-soon.js
+++
@@ -1,3 +0,0 @@
-(function() {
-    document.body.innerHTML = 'Coming Soon...';
-}());
--- assets/scripts/loader.js
+++
@@ -1,5 +0,0 @@
-var a = ['Web developer','speed','html','css'];
-var i= 0;
-setInterval(function() {
-    document.getElementById('f').innerHTML = a[i];
-},1000);
